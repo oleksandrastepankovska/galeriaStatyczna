fc37be4081c8051a21295836e986f99b719496c7
heavycavalry <klaudia.diana.paluch@gmail.com>
2020-11-29 17:05:02 +0100

    fin

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 function klaudia() {
 
-    const prev = document.querySelector(".prev");
-    const next = document.querySelector(".next");
+    const urls = ["https://cdn.pixabay.com/photo/2019/11/04/07/48/pumpkins-4600419_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2014/11/21/15/45/geese-540663_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2018/06/26/08/56/living-room-3498914_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2019/05/08/18/23/pipes-4189258_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2020/06/21/09/48/hill-5324149_960_720.jpg"
+    ]
 
-    let current = document.getElementById("one");
+
+    let section = document.querySelector(".klaudia")
+    section.querySelectorAll('.slider-box').forEach(console.log)
+
+
+    function createImage(url) {
+        const img = new Image();
+        img.src = url;
+        const slider = section.querySelector('.slider-box');
+        let imgBox = document.createElement("div");
+        imgBox.classList.add("slide-img");
+        console.dir(imgBox);
+        slider.appendChild(imgBox);
+        imgBox.appendChild(img);
+    }
+
+    for (el in urls) {
+        createImage(urls[el]);
+    }
+
+    const prev = section.querySelector(".prev");
+    const next = section.querySelector(".next");
+
+
+    const slider = section.querySelector('.slider-box');
+    let current = slider.children[0];
 
     next.addEventListener("click", nextImg);
     prev.addEventListener("click", prevImg);
