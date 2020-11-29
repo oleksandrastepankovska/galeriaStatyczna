1b0da31fc49941d3cb54be1cff65814d55e6adb0
miceyjuice <ganjafz@gmail.com>
2020-11-29 15:48:43 +0100

    Image loading updated :fried_shrimp:

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const galleryMaker = (images) => {
+const ps_galleryMaker = (images) => {
   if (!images.length) return;
 
-  const imgDiv = document.querySelector(".gallery");
+  const imgDiv = document.querySelector(".ps_gallery");
 
   const img = new Image();
   const url = images.shift();
 
-  img.onload = () => galleryMaker(images);
+  img.onload = () => ps_galleryMaker(images);
   img.src = url;
   imgDiv.appendChild(img);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  galleryMaker([
+  ps_galleryMaker([
     "https://picsum.photos/500/500?random=1",
     "https://picsum.photos/500/500?random=2",
     "https://picsum.photos/500/500?random=3",
