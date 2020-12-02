4172bc873b277f462e0e208c31f0f5d8aae541cc
Marcelina Sitko <marcelina.sitko@interia.pl>
2020-12-02 22:16:37 +0100

    done

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-function klaudia() {
+function marcelina(images){
+    if(images.length == 0) return;
+    const divForImg = document.querySelector('.galleryBox');
 
-    const urls = ["https://cdn.pixabay.com/photo/2014/11/21/15/45/geese-540663_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2019/11/04/07/48/pumpkins-4600419_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2018/06/26/08/56/living-room-3498914_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2019/05/08/18/23/pipes-4189258_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2020/06/21/09/48/hill-5324149_960_720.jpg"
-    ]
+       const img = new Image();
+       const url = images.shift();
+       img.addEventListener("load", marcelina(images));
 
-    let section = document.querySelector(".klaudia")
-
-    function createImage(url, index) {
-        const img = new Image();
-        img.src = url;
-        const slider = section.querySelector('.slider');
-        img.addEventListener('load', event => loadImg(index + 1))
-        slider.appendChild(img);
-    }
-
-    function loadImg(index) {
-        if (index < urls.length) {
-            createImage(urls[index], index)
-        }
-    }
-
-    loadImg(0)
-
+       img.src = url;
+       divForImg.appendChild(img);
 }
-
-klaudia();
+document.addEventListener("DOMContentLoaded", () => {
+    marcelina(
+    [
+        "https://picsum.photos/400/400?random=1",
+        "https://picsum.photos/400/400?random=2",
+        "https://picsum.photos/400/400?random=3",
+        "https://picsum.photos/400/400?random=4",
+        "https://picsum.photos/400/400?random=5"
+       
+    ]);
+});
